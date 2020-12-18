a61dcea366efe718f9e86438660875de014e84ba
JakeY2k <60342351+JakeY2k@users.noreply.github.com>
2020-12-18 19:58:17 +0000

    Add files via upload

---
+++ The Software Guys Project/Js/SoftwareGuy.js
@@ -0,0 +1,35 @@
+
+(function(){
+	
+    document.getElementById('buybutton').addEventListener("click", myButton, false);
+  
+}());
+ 
+
+
+
+
+
+
+
+
+
+
+
+function myParas()
+{
+	
+	var newParas = document.getElementById('buybuttons');
+
+	var newParas2 = document.createElement("p");
+	
+	var newText = document.createTextNode("New Paragraph for this assignment #1");
+
+	newParas.appendChild(newText);
+	newDivs.appendChild(newPara);
+    
+    
+   
+}
+
+	
